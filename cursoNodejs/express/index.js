0dafb08611fb65cdcfffc050404367a9b6c62b91
Anderson Leite <andersonleite.pl013@academico.ifsul.edu.br>
2024-10-08 22:47:41 -0300

    mobile development

--- cursoNodejs/express/index.js
+++ cursoNodejs/express/index.js
@@ -2,8 +2,17 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-app.use(express.static(path.join(__dirname, 'client')))
+app.use("/meusite", express.static(path.join(__dirname, 'client')))
 
+app.get("/", (req, res) => {
+    //res.set("Content-Type", "text/html")
+    res.type("json")
+    res.send("<h1>Hello World from get</>")
+})
+
+app.post("/", (req, res) => {
+    res.send("<h1>Hello World from get</>")
+})
 const port = 5000
 
 app.listen(port, () => {
