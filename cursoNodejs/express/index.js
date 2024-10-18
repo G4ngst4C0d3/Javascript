5bf7b3fa50077d75497de122544c9b1ad89d234e
AndersonPedrosoLeite <andersonleite.pl013@academico.ifsul.edu.br>
2024-10-18 17:34:26 -0300

    node templete engine

--- cursoNodejs/express/index.js
+++ cursoNodejs/express/index.js
@@ -1,13 +1,37 @@
 const express = require('express')
+const fs = require('fs')
 const path = require('path')
 
 const app = express()
+
+let user = {
+    id: 1,
+    nome: "DaSwag Outlet",
+    email: "daswagoutlet@contato.com.br",
+    telefone: "(53)3734-7343"
+}
 app.use("/meusite", express.static(path.join(__dirname, 'client')))
 
 app.get("/", (req, res) => {
-    //res.set("Content-Type", "text/html")
+    /*res.set("Content-Type", "text/html")
     res.type("json")
     res.send("<h1>Hello World from get</>")
+    */
+
+    function render(data, obj){
+        for(let key in obj){
+            data = data.replace(`{{{${key}}}}`, obj[key])
+        }
+        
+        return data 
+    }
+   fs.readFile('./index.html', 'UTF8', (err, data) => {
+        if(!err){
+
+
+            res.send(render(data, user))
+        }
+   })
 })
 
 app.post("/", (req, res) => {
